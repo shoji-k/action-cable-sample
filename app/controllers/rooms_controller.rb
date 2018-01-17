ef7a2697b28240886631cd58b71e337345c29e14
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2018-01-17 13:09:16 +0900

    json api test sample

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,14 @@ class RoomsController < ApplicationController
   def show
     @messages = Message.all
   end
+
+  def index
+    render json: {
+      id: 10,
+      messages: [
+        {id: 1, content: 'hey'},
+        {id: 2, content: 'yo'}
+      ]
+    }
+  end
 end
--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -6,4 +6,13 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get index" do
+    get rooms_index_url, as: :json
+    json = JSON.parse(response.body)
+
+    assert_equal 10, json['id']
+
+    expected = {'id' => 1, 'content' => 'hey'}
+    assert_equal expected, json['messages'][0]
+  end
 end
