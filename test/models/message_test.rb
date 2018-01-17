61b61c2bafde05f8d5878690f54c09c1b6b77495
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2018-01-17 15:49:09 +0900

    model test sample

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @two = messages(:two)
+  end
+
+  def teardown
+    @two = nil
+  end
+
+  test "save message" do
+    message = Message.new({
+      content: 'hey'
+    })
+    assert message.save
+  end
+  test "test fixture count" do
+    messages = Message.all
+    assert_equal 2, messages.count
+  end
+
+  test "test one fixture" do
+    message = Message.find_by(content: 'MySecond')
+    assert_equal @two, message
+    assert_equal 'MySecond', message.content
+  end
 end
